3182d6cb62c73265c4c09f4b4ba076ba4d19b019
Pratham Singh <112713687+pratham1singh@users.noreply.github.com>
2023-10-06 23:40:08 +0530

    Added solution - LeetHub

---
+++ Reverse alternate nodes in Link List - GFG/reverse-alternate-nodes-in-link-list.java
@@ -0,0 +1,103 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Rearr
+{
+    static Node head;
+    
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t  =sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            Node temp = head;
+            for(int i = 1; i < n; i++)
+            {
+                int a = sc.nextInt();
+                temp.next = new Node(a);
+                temp = temp.next;
+            }
+            
+            Solution ob = new Solution();
+            ob.rearrange(head);
+            printLast(head);
+            System.out.println();
+        }
+    }
+    
+    public static void printLast(Node node)
+    {
+        while(node != null)
+        {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*node class of the linked list
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}*/
+class Solution
+{
+    public static Node reverse(Node head,Node pre){
+        if(head==null)
+            return pre;
+        else{
+            Node forw=head.next;
+            head.next=pre;
+            return reverse(forw,head);
+           
+        }
+    }
+    public static void rearrange(Node odd)
+    {
+        Node head2=new Node(-1),tail=head2,ptr=odd.next,pre=odd;
+        
+        while(ptr!=null&&ptr.next!=null){
+        
+            Node forw=ptr.next;
+            tail.next=ptr;
+            tail=ptr;
+            pre.next=ptr.next;
+            ptr.next=null;
+            pre=forw;
+            ptr=forw.next;
+        }
+        head2=head2.next;
+        head2=reverse(head2,null);
+        ptr=odd;
+        while(ptr.next!=null)
+            ptr=ptr.next;
+        ptr.next=head2;
+        
+        
+        
+    }
+}
