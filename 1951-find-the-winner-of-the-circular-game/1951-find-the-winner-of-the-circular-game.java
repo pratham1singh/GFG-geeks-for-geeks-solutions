02bc762d1f68017df482768e53fe5de1cfb91049
Pratham Singh <112713687+pratham1singh@users.noreply.github.com>
2024-07-08 16:06:24 +0530

    Time: 5 ms (36.43%), Space: 40.8 MB (36.96%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -1,5 +1,5 @@
 //Solution1 using simple array
-class Solution {
+class Solution1 {
     public int findTheWinner(int n, int k) {
         int arr[]= new int[n],i=0,c=n;
 
@@ -36,5 +36,47 @@ class Solution {
         return i+1;
 
 
+    }
+}
+
+
+//Solution2 using linkedlist
+class Solution {
+    class node{
+        int data;
+        node pre,forw;
+
+        public node(int data){
+            this.data=data;
+            this.pre=null;
+            this.forw=null;
+        }
+    }
+
+    public int findTheWinner(int n, int k) {
+        node head=new node(1),ptr=head;
+        for(int i=2;i<=n;i++){
+            node temp= new node(i);
+            temp.pre=ptr;
+            ptr.forw=temp;
+            ptr=temp;
+        }
+        ptr.forw=head;
+        head.pre=ptr;
+        ptr=head;
+
+        while(ptr.pre!=ptr){
+            int count=1;
+            while(count<k){
+                count+=1;
+                ptr=ptr.forw;
+            }
+            //delete node
+            // System.out.println(ptr.data);
+            ptr.forw.pre=ptr.pre;
+            ptr.pre.forw=ptr.forw;
+            ptr=ptr.forw;
+        }
+        return ptr.data;
     }
 }
